7972980192e292bd6a8d6558104e96ba4df9eed2
SHIN SUJUNG <shinssjj79@gmail.com>
2025-01-12 11:33:38 +0900

    Initial commit

---
+++ src/main/java/com/example/timer_alarm_app/TimerAlarmAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.timer_alarm_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimerAlarmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimerAlarmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/timer_alarm_app/TimerAlarmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timer_alarm_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimerAlarmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
